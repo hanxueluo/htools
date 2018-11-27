f561938149be04299c212a7606368b8929f5ebc5
Huanle Han <hanxueluo@gmail.com>
2018-11-27 19:51:47 +0800

    hetct: wrapper etcdctl

---
+++ hetctl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CA=/etc/kubernetes/etcd/ca.crt
+CRT=/etc/kubernetes/etcd/etcd.crt
+KEY=/etc/kubernetes/etcd/etcd.key
+BIN=/usr/local/etcd/bin/etcdctl
+
+if test -f $CA && test -f $CRT && test -f $KEY;then
+    :
+else
+    CA=/etc/kubernetes/pki/etcd/ca.crt
+    CRT=/etc/kubernetes/pki/apiserver-etcd-client.crt
+    KEY=/etc/kubernetes/pki/apiserver-etcd-client.key
+fi
+
+if which $BIN >/dev/null;then
+    :
+else
+    BIN=etcdctl
+fi
+
+_etcdctl() {
+	ETCDCTL_API=3 $BIN \
+		--cacert $CA \
+		--cert $CRT \
+		--key $KEY \
+		"$@"
+}
+
+_etcdctl "$@"
+
+
