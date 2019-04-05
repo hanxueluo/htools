83ab9023b66e4247495648ee8b20ea81a871b4f0
Huanle Han <hanxueluo@gmail.com>
2019-04-05 16:32:52 +0800

    copyhtools: copy lightweight htools
    Signed-off-by: Huanle Han <hanxueluo@gmail.com>

---
+++ copyhtools.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if test -z "$1";then
+    echo "Error arguments:"
+    echo "   $0 IP [PASSWORD]"
+    exit 1
+fi
+
+list_file() {
+    cat <<EOF
+#calicoctl
+#etcdctl
+#ipvsadm
+#ixcalc
+#kubectl
+*.sh
+*.py
+alias_pykube
+hovs
+hss
+hguess
+kube_alias
+log_cni
+pykube
+vvm
+EOF
+}
+
+rm -rf /tmp/htools/
+mkdir /tmp/htools
+cp -a `list_file |grep -v '^#'|xargs` /tmp/htools/
+
+./hss u "$1" /tmp/htools/ /root/
+exec ./hss "$1"
